d76075edaaaf7fbc7c782dfc44230fe7afdc38ae
lZh1r <electroxhackerpig09@gmail.com>
2025-04-20 15:46:50 +0300

    removed unused UseEffect import from Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import {ChangeEvent, FormEvent, useEffect, useState} from "react";
+import {ChangeEvent, FormEvent, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHeart, faUser} from "@fortawesome/free-solid-svg-icons";
 import {Link, useNavigate} from "react-router";
