1df7713ab39942d768247424a2bf228d607a2bae
lZh1r <electroxhackerpig09@gmail.com>
2025-04-20 15:19:14 +0300

    login done

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,15 @@
+
+function SignUp() {
+    return (
+        <div>
+            <form>
+                <input type="text"/>
+                <input type="email"/>
+                <input type="password"/>
+                <input type="password"/>
+            </form>
+        </div>
+    );
+}
+
+export default SignUp;
