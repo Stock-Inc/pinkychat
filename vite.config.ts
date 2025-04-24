e555ddc5db62a09c809a40d9f528b4ab76c5ffc2
lZh1r <electroxhackerpig09@gmail.com>
2025-04-24 21:05:33 +0300

    vite config and some needed libs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+
+  }
+});
