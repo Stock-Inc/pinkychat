aa6ff6236eaa9698447e5e17ac9f996b9e65b121
lZh1r <electroxhackerpig09@gmail.com>
2025-04-29 11:37:00 +0300

    show previous message placeholder in chat button

--- src/elements/side-modal/MenuModal.tsx
+++ src/elements/side-modal/MenuModal.tsx
@@ -4,10 +4,10 @@ function MenuModal() {
 
     return (
         <div role="dialog" className="border-r-2 border-r-gray-800 bg-sub-dark text-femboy fixed h-screen">
-            <ChatButton chatName="Mark"/>
-            <ChatButton chatName="Group"/>
-            <ChatButton chatName="Test"/>
-            <ChatButton chatName="Hello World"/>
+            <ChatButton chatName="Mark" previousMessage={"Hey!"}/>
+            <ChatButton chatName="Group" previousMessage={"That was cool"}/>
+            <ChatButton chatName="Test" previousMessage={"..."}/>
+            <ChatButton chatName="Hello World" previousMessage={"..."}/>
         </div>
     );
 }
